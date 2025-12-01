a22ca341abf0f49f260fb143e1b4d06c13c1224b
Calebapril <calebessien001@gmail.com>
2025-12-01 13:10:38 +0100

    import showToast to my editMedia folder

--- app/(root)/(admin)/admin/media/edit/[id]/page.jsx
+++ app/(root)/(admin)/admin/media/edit/[id]/page.jsx
@@ -1,103 +1,106 @@
-'use client'
+"use client";
 import BreadCrumb from "@/components/Application/Admin/BreadCrumb";
 import ButtonLoading from "@/components/Application/ButtonLoading";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import useFetch from "@/hooks/useFetch"
+import useFetch from "@/hooks/useFetch";
 import { zSchema } from "@/lib/zodSchema";
-import { ADMIN_DASHBOARD, ADMIN_MEDIA_SHOW } from "@/routes/AdminPanelRoute"
+import { ADMIN_DASHBOARD, ADMIN_MEDIA_SHOW } from "@/routes/AdminPanelRoute";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
-import { use, useEffect, useState } from 'react'
+import { use, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import imgPlaceholder from '@/public/img-placeholder.webp'
+import imgPlaceholder from "@/public/img-placeholder.webp";
 import axios from "axios";
+import { showToast } from "@/lib/showToast";
 
 const breadCrumbData = [
   {
     href: ADMIN_DASHBOARD,
-    label: 'Home'
+    label: "Home",
   },
   {
     href: ADMIN_MEDIA_SHOW,
-    label: 'Media'
+    label: "Media",
   },
   {
     href: "",
-    label: 'Edit Media'
+    label: "Edit Media",
   },
 ];
 
 const EditMedia = ({ params }) => {
-  const { id } = use(params)
-  const {data: mediaData} = useFetch(`/api/media/get/${id}`)
-  const [loading, setLoading] = useState(false)
+  const { id } = use(params);
+  const { data: mediaData } = useFetch(`/api/media/get/${id}`);
+  const [loading, setLoading] = useState(false);
 
+  const formSchema = zSchema.pick({
+    _id: true,
+    alt: true,
+    title: true,
+  });
 
-    const formSchema = zSchema
-      .pick({
-        _id: true,
-        alt: true,
-        title: true
-      })
-  
-    const form = useForm({
-      resolver: zodResolver(formSchema),
-      defaultValues: {
-        _id: "",
-        alt: "",
-        title: "",
-      },
-    });
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      _id: "",
+      alt: "",
+      title: "",
+    },
+  });
 
-    useEffect(()=>{
-      if(mediaData && mediaData.success){
-        const data = mediaData.data
-        form.reset({
-          _id: data._id,
-          alt: data.alt,
-          title: data.title
-        })
+  useEffect(() => {
+    if (mediaData && mediaData.success) {
+      const data = mediaData.data;
+      form.reset({
+        _id: data._id,
+        alt: data.alt,
+        title: data.title,
+      });
+    }
+  }, [mediaData]);
+
+  const onSubmit = async (values) => {
+    try {
+      setLoading(true);
+      const { data: response } = await axios.put("/api/media/update", values);
+      if (!response.success) {
+        throw new Error(response.message);
       }
-    },[mediaData])
-  
-    const onSubmit = async (values) => {
-          try {
-        setLoading(true)
-        const {data: response} = await axios.put('/api/media/update', values)
-        if(!response.success){
-          throw new Error(response.message)
-        }
 
-        showToast('success', response.message)
-      } catch (error) {
-        showToast('error', error.message)
-      }finally{
-        setLoading(false)
-      }
-    };
-
+      showToast("success", response.message);
+    } catch (error) {
+      showToast("error", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div>
       <BreadCrumb breadcrumbData={breadCrumbData} />
 
-
-            <Card className="py-0 rounded shadow-sm">
-              <CardHeader className="pt-3 px-3 border-b [.border-b]:pb-2">
-                <h4 className="text-xl font-semibold">Edit Media</h4>
-              </CardHeader>
-              <CardContent className="pb-5">
-      
-            <Form {...form}>
+      <Card className="py-0 rounded shadow-sm">
+        <CardHeader className="pt-3 px-3 border-b [.border-b]:pb-2">
+          <h4 className="text-xl font-semibold">Edit Media</h4>
+        </CardHeader>
+        <CardContent className="pb-5">
+          <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}>
               <div className="mb-5">
                 <Image
-                src={mediaData?.data?.secure_url || imgPlaceholder}
-                width={150}
-                height={150}
-                alt={mediaData?.alt || 'Image'}
+                  src={mediaData?.data?.secure_url || imgPlaceholder}
+                  width={150}
+                  height={150}
+                  alt={mediaData?.alt || "Image"}
                 />
               </div>
               <div className="mb-5">
@@ -108,11 +111,7 @@ const EditMedia = ({ params }) => {
                     <FormItem>
                       <FormLabel>Alt</FormLabel>
                       <FormControl>
-                        <Input
-                          type="text"
-                          placeholder="Enter alt"
-                          {...field}
-                        />
+                        <Input type="text" placeholder="Enter alt" {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -148,15 +147,12 @@ const EditMedia = ({ params }) => {
                   className="cursor-pointer"
                 />
               </div>
-
             </form>
           </Form>
-
-              </CardContent>
-            </Card>
-    
+        </CardContent>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default EditMedia
+export default EditMedia;
